b93bafb8449de98634bac5932be9339b20fe3499
Al Ilseman <ilsemanal@johndeere.com>
2017-09-08 08:49:28 -0500

    fixed promise

--- index.js
+++ index.js
@@ -6,11 +6,12 @@ const path = require('path');
 
 const args = process.argv;
 
-recursive(path.resolve(args[2])).then(data => {
-  if (err) {
-    console.error(err);
-  }
-  data.forEach(file => {
-    console.log(file);
+recursive(path.resolve(args[2]))
+  .then(data => {
+    data.forEach(file => {
+      console.log(file);
+    });
+  })
+  .catch(err => {
+    console.errror(err);
   });
-});
